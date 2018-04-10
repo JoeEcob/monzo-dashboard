2fd0557bd1649cb7b8e958f611772778a35e0f8c
Joe Ecob <me@joeecob.uk>
2018-04-10 22:57:22 +0100

    Persist state to local storage

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-import 'normalize.css/normalize.css'
-import 'milligram/dist/milligram.css'
-import 'font-awesome/css/font-awesome.css'
-import './css/app.css'
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import App from './components/App';
+import { loadState, saveState } from './localStorage';
+import registerServiceWorker from './registerServiceWorker';
+import throttle from 'lodash/throttle';
+import 'normalize.css/normalize.css';
+import 'milligram/dist/milligram.css';
+import 'font-awesome/css/font-awesome.css';
+import './css/app.css';
 
+const persistedStore = loadState();
 const store = createStore(
   reducer,
+  persistedStore,
   applyMiddleware(thunk)
-)
+);
+
+store.subscribe(throttle(() => {
+  const { accessToken, accounts, transactionsByAccountId } = store.getState();
+
+  saveState({
+    accessToken,
+    accounts,
+    transactionsByAccountId
+  });
+}, 1000));
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
 
-registerServiceWorker()
+registerServiceWorker();
