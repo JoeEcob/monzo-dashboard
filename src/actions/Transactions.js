06c01b3e2a9d9d870c1ccc1dbf6d5ab77e5b23d9
Joe Ecob <me@joeecob.uk>
2018-05-28 22:10:31 +0100

    Fetch transactions if last fetch was over 6 hours ago

--- src/actions/Transactions.js
+++ src/actions/Transactions.js
@@ -1,46 +1,47 @@
 import MonzoApi from 'monzo-api'
 
 export const REQUEST_TRANSACTIONS = 'REQUEST_TRANSACTIONS'
-export const RECEIVE_TRANSACTIONS = 'RECEIVE_TRANSACTIONS'
-export const REJECT_TRANSACTIONS = 'REJECT_TRANSACTIONS'
+export const RECEIVE_TRANSACTIONS = 'RECEIVE_TRANSACTIONS';
+export const REJECT_TRANSACTIONS = 'REJECT_TRANSACTIONS';
 
 export const requestTransactions = accountId => ({
   type: REQUEST_TRANSACTIONS,
   accountId
-})
+});
 
 export const receiveTransactions = (accountId, json) => ({
   type: RECEIVE_TRANSACTIONS,
   accountId,
   transactions: json.transactions,
   receivedAt: Date.now()
-})
+});
 
 export const rejectTransactions = (accountId, error) => ({
   type: REJECT_TRANSACTIONS,
   receivedAt: Date.now(),
   accountId,
   error
-})
+});
 
 const fetchTransactions = (accessToken, accountId) => dispatch => {
-  dispatch(requestTransactions(accountId))
+  dispatch(requestTransactions(accountId));
 
-  const api = new MonzoApi()
-  api.accessToken = accessToken
+  const api = new MonzoApi();
+  api.accessToken = accessToken;
 
   return api.transactions(accountId, true)
     .then(
       json => dispatch(receiveTransactions(accountId, json)),
       error => dispatch(rejectTransactions(accountId, error.response.body))
-    )
-}
+    );
+};
 
 export const fetchTransactionsIfNeeded = accountId => (dispatch, getState) => {
-  const state = getState()
-  const transactions = state.transactionsByAccountId[accountId]
+  const state = getState();
+  const transactions = state.transactionsByAccountId[accountId];
+  const maxTimeDifference = 21600000; // 6 hours in ms
 
-  if (!transactions) {
-    return dispatch(fetchTransactions(state.accessToken, accountId))
+  if (!transactions || transactions.lastUpdated < (Date.now() - maxTimeDifference)) {
+    return dispatch(fetchTransactions(state.accessToken, accountId));
   }
-}
+};
--- src/actions/__tests__/Transactions.spec.js
+++ src/actions/__tests__/Transactions.spec.js
@@ -1,14 +1,14 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import * as module from '../Transactions'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as module from '../Transactions';
 
-Date.now = jest.fn(() => 1483228800000)
-const mockStore = configureMockStore([thunk])
-const accountId = 'account123'
+Date.now = jest.fn(() => 1483228800000);
+const mockStore = configureMockStore([thunk]);
+const accountId = 'account123';
 
 describe('Actions::Transactions', () => {
   it('creates a receive action when fetching transactions is finished', () => {
-    const accessToken = 'token123'
+    const accessToken = 'token123';
     const expectedActions = [
       { type: module.REQUEST_TRANSACTIONS, accountId},
       {
@@ -23,37 +23,59 @@ describe('Actions::Transactions', () => {
         ],
         receivedAt: 1483228800000
       }
-    ]
+    ];
 
     const store = mockStore({
       accessToken,
       transactionsByAccountId: { }
-    })
+    });
 
     return store.dispatch(module.fetchTransactionsIfNeeded(accountId)).then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
-    })
-  })
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
 
   it('skips fetching transactions if items already exist', () => {
-    const accessToken = 'token123'
+    const accessToken = 'token123';
     const store = mockStore({
       accessToken,
       transactionsByAccountId: { [accountId]: { items: ['foo'], isFetching: false } }
-    })
+    });
 
-    store.dispatch(module.fetchTransactionsIfNeeded(accountId))
-    expect(store.getActions()).toEqual([])
-  })
+    store.dispatch(module.fetchTransactionsIfNeeded(accountId));
+    expect(store.getActions()).toEqual([]);
+  });
 
   it('skips fetching transactions if already fetching', () => {
-    const accessToken = 'token123'
+    const accessToken = 'token123';
     const store = mockStore({
       accessToken,
       transactionsByAccountId: { [accountId]: { items: [], isFetching: true } }
-    })
+    });
 
-    store.dispatch(module.fetchTransactionsIfNeeded(accountId))
-    expect(store.getActions()).toEqual([])
-  })
-})
+    store.dispatch(module.fetchTransactionsIfNeeded(accountId));
+    expect(store.getActions()).toEqual([]);
+  });
+
+  it('fetches transactions if past expiry date', () => {
+    const accessToken = 'token123';
+    const store = mockStore({
+      accessToken,
+      transactionsByAccountId: {
+        [accountId]: {
+          items: [],
+          isFetching: true,
+          lastUpdated: 1483207199999 // 6 hours and 1 ms before
+        }
+      }
+    });
+
+    store.dispatch(module.fetchTransactionsIfNeeded(accountId));
+    expect(store.getActions()).toEqual([
+      {
+        accountId,
+        type: module.REQUEST_TRANSACTIONS
+      }
+    ]);
+  });
+});
