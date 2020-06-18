2ea6bd507b3478f21eff23c58256ccddce94aa9a
Joe Ecob <me@joeecob.uk>
2020-06-18 18:43:44 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <small>Made with <span className="fa fa-heart">&nbsp;</span> in Manchester | </small>
       <small><a href="https://gitlab.com/JoeEcob/monzo-dashboard" title="View on GitLab">View on GitLab</a></small>
     </div>
   </footer>
